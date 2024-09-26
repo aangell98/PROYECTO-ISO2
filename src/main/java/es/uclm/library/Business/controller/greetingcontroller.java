da1ac8c1623a9dbbaecab2fe70e8306f8cefb919
Angel <angelluislara@hotmail.es>
2024-09-26 12:02:26 +0200

    Pruebas pendientes de implementar

---
+++ src/main/java/es/uclm/library/Business/controller/greetingcontroller.java
@@ -0,0 +1,23 @@
+package es.uclm.library.Business.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import es.uclm.library.Business.entity.Greeting;
+
+@Controller
+public class greetingcontroller {
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+}
