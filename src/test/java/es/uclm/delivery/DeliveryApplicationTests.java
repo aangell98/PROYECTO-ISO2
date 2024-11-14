e8f20523bf3e46a129a59666f1ed7b27da7758a7
aangell98 <angelluislara@hotmail.es>
2024-11-14 11:54:56 +0100

    test2

--- src/test/java/es/uclm/delivery/DeliveryApplicationTests.java
+++ src/test/java/es/uclm/delivery/DeliveryApplicationTests.java
@@ -3,7 +3,7 @@ package es.uclm.delivery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DeliveryApplication.class)
 class DeliveryApplicationTests {
 
 	@Test
