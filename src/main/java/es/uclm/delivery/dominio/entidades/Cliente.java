6af767ebe87a93210bcffefea3bb0f6d61595a26
aangell98 <angelluislara@hotmail.es>
2024-11-19 01:10:36 +0100

    el cliente puede guardar direcciones y seleccionar las asociadas a su usuario a la hora de realizar un pedido

--- src/main/java/es/uclm/delivery/dominio/entidades/Cliente.java
+++ src/main/java/es/uclm/delivery/dominio/entidades/Cliente.java
@@ -15,6 +15,7 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "nombre", nullable = false)
--- src/main/java/es/uclm/delivery/dominio/entidades/Direccion.java
+++ src/main/java/es/uclm/delivery/dominio/entidades/Direccion.java
@@ -12,6 +12,7 @@ public class Direccion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "calle", nullable = false)
